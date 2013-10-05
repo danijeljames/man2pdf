857410750ec846ba1b19730f852a239cd028a728
Danijel James <danijel.james@me.com>
2013-10-05 23:30:44 +1000

    Rewrote script, v2.0 release
    Released v2.0. Total re-write of the whole script. Added built-in
    ReadMe to file, with usage explained simply.

--- man2pdf.sh
+++ man2pdf.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
-# Bash shell script to open man pages in Preview
-# so that later user can save them to PDF
-# Created by Danijel J <http://www.danijelj.com>, under MIT..
-# ----------------------------------------------------------------------
 
-man -t $1 | open -f -a /Applications/Preview.app
-# osascript -e 'tell "Preview" 
+version="2.0"
+
+###############################################################################
+# Load All Functions                                                          #
+###############################################################################
+
+# Show script help
+function showHelp {
+        clear
+        echo "man2pdf $version"
+        echo 
+        echo 'Usage: $1 [-h | manpage]'
+        echo 
+        echo 'Option:'
+        echo '   -h         This help file'
+        echo '   manpage    Name of the manpage to open in Preview'
+        echo 
+}   # end of showHelp
+
+
+# Pipe manpage to Preview
+function manPipe {
+        man -t "${1}" | open -f -a /Applications/Preview.app
+}   # end of manPipe
+
+
+###############################################################################
+# Standard UI                                                                 #
+###############################################################################
+
+case $1 in
+        -h | --help )           showHelp
+                                exit
+                                ;;
+        * )                     manPipe
+                                exit 1
+esac
