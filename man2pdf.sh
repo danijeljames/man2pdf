59d166f31bb6ac4019d9181a9649961e29c43290
Danijel J <danijel.james@gmail.com>
2013-10-19 19:52:00 +1100

    updated to v2.0.1

--- man2pdf.sh
+++ man2pdf.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
-version="2.0"
+version="2.0.1"
 
 ###############################################################################
 # Load All Functions                                                          #
 ###############################################################################
+
+# Check if man page installed
+function manExist {
+	if [ ! -f /usr/share/man/man1/man2pdf.1 ]
+	then
+	    sudo curl "https://raw.github.com/danijeljames/man2pdf/master/man2pdf.1" -o "/usr/share/man/man1/man2pdf.1"
+		echo "Man page for man2pdf now installed!"
+		showHelp
+	else
+		echo "Man page already installed!"
+		showHelp
+	fi
+}
+
 
 # Show script help
 function showHelp {
         clear
         echo "man2pdf $version"
         echo 
-        echo 'Usage: $1 [-h | manpage]'
+        echo 'Usage: $1 [-h | manpage | -i]'
         echo 
         echo 'Option:'
-        echo '   -h         This help file'
-        echo '   manpage    Name of the manpage to open in Preview'
+        echo '   -h          This help file'
+        echo '   -i          Auto-install manpage (requires sudo)'
+        echo '   manpage     Name of the manpage to open in Preview'
         echo 
 }   # end of showHelp
 
@@ -34,6 +49,9 @@ case $1 in
         -h | --help )           showHelp
                                 exit
                                 ;;
+        -i )					manExist
+        						exit
+        						;;
         * )                     manPipe
                                 exit 1
 esac
