337c440f04e07bfadf543f8b8fe09e5f1464d59c
Danijel J <danijel.james@gmail.com>
2014-01-13 22:08:10 +1100

    v3.0.0 update
    Updated to 3.0.0

--- man2pdf.sh
+++ man2pdf.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
+#
+# Copyright (c) 2013-2014 Danijel James.
+#
+# You may distribute under the terms of the MIT License
+#
+#
+# Portions of code are released under GNU Public License:
+# Copyright (c) 1990, 1991, John W. Eaton.
+# Copyright (c) 1994-1999, Andries E. Brouwer.
+#
+#
+program=`basename $0`
 
-version="2.0.1"
+version="3.0.0"
 
-###############################################################################
-# Load All Functions                                                          #
-###############################################################################
+if [ $# = 0 ]
+then
+    showHelp
+    exit 1
+fi
+
+manpath=`man --path | tr : '\040'
+
+if [ "$manpath" = "" ]
+then
+    echo "$program: manpath is null"
+    exit 1
+fi
 
 # Check if man page installed
 function manExist {
@@ -19,21 +41,20 @@ function manExist {
 	fi
 }
 
-
 # Show script help
 function showHelp {
         clear
         echo "man2pdf $version"
         echo 
-        echo 'Usage: $1 [-h | manpage | -i]'
+        echo 'Usage: $program [manpage|-h|-i]'
         echo 
         echo 'Option:'
-        echo '   -h          This help file'
-        echo '   -i          Auto-install manpage (requires sudo)'
         echo '   manpage     Name of the manpage to open in Preview'
+        echo '   -i          Auto-install manpage (requires sudo)'
+        echo '   -h          This help file'
+        echo '   -v          Show version number'
         echo 
 }   # end of showHelp
-
 
 # Pipe manpage to Preview
 function manPipe {
@@ -41,17 +62,43 @@ function manPipe {
 }   # end of manPipe
 
 
-###############################################################################
-# Standard UI                                                                 #
-###############################################################################
+args=
+for arg in $*; do
+    case $arg in
+        -H|-h)
+		showHelp
+	    exit 0
+	    ;;
+        -i|-I)
+		manExist
+	    exit 0
+	    ;;
+	    --version|-V|-v)
+	    echo "$program $version"
+	    exit 0
+	    ;;
+		-*)
+	    echo "$program: $arg: unknown option given"
+	    exit 1
+	    ;;
+		*)
+	    args="$args $arg"
+    esac
+done
 
-case $1 in
-        -h | --help )           showHelp
-                                exit
-                                ;;
-        -i )					manExist
-        						exit
-        						;;
-        * )                     manPipe
-                                exit 1
-esac
+while [ "$1" != "" ]
+do
+    found=0
+    for d in /var/cache/man $manpath /usr/lib
+    do
+        if [ -f $d/whatis ]
+        man -t "${1}" | open -f -a /Applications/Preview.app
+        fi
+    done
+
+    if [ $found = 0 ]
+    then
+        echo "$1: nothing appropriate"
+    fi
+
+exit
